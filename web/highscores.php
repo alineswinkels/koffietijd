711a7d2de65de06ef35193f64e8fdb5f4eb5e078
Sietse Radstake <sietse@devserver.freshheads.local>
2015-08-14 13:58:39 +0200

    Coffee

---
+++ web/highscores.php
@@ -0,0 +1,85 @@
+<?php
+	include('header.php');
+?>
+<h1>De tussenstand</h1>
+<section class='rank'>
+	<div class='rank_name'>
+		<img class='rank_icon' src='img/coffee.png'/>
+		<h1>De meeste koffie gedronken</h1>
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Sietse_Radstake.jpg')" alt='sietse'></div>
+		<h2>Sietse_Radstake</h2>
+		<h3>Coffee addict 1'</h3>
+		<div class='rank_description'>
+			<!-- <div class='rank_icon coffee'></div> -->
+			<p>100 <br/>
+			<span class='point_despription'>koffie</span></p>
+		</div>
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Dennis_van_Meel.jpg')" alt='sietse'></div>
+		<h2>Dennis_van_Meel</h2>
+		<h3>Coffee addict 2'</h3>
+		<div class='rank_description'>
+			<!-- <div class='rank_icon coffee'></div> -->
+			<p>80</p>
+		</div>
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Etienne_van_Breugel.jpg')" alt='sietse'></div>
+		<h2>Etienne_van_Breugel</h2>
+		<h3>Coffee addict 3'</h3>
+		<div class='rank_description'>
+			<!-- <div class='rank_icon coffee'></div> -->
+			<p>65</p>
+		</div>
+	</div>
+</section>
+<section class='rank'>
+		<div class='headrank'>
+		<div class='head' style="background-image:url('img/Sietse_Radstake.jpg')" alt='sietse'></div>
+		<h2>Sietse_Radstake</h2>
+		<h3>Butler 1'</h3>
+		<div class='rank_description'>
+			<!-- <div class='rank_icon butler'></div> -->
+			<p>100</p>
+		</div>
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Martin_Broos.jpg')" alt='sietse'></div>
+		<h2>Martin_Broos</h2>
+		<h3>Butler 2'</h3>
+		<div class='rank_description'>
+			<!-- <div class='rank_icon butler'></div> -->
+			<p>100</p>
+		</div>
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Rik_Deuzeman.jpg')" alt='sietse'></div>
+		<h2>Rik_Deuzeman</h2>
+		<h3>Butler 3'</h3>
+		<div class='rank_description'>
+			<!-- <div class='rank_icon butler'></div> -->
+			<p>100</p>
+		</div>
+	</div>
+</section>
+<section class='rank'>
+		<div class='headrank'>
+		<div class='head' style="background-image:url('img/Stefan_Moonen.jpg')" alt='sietse'></div>
+		<h2>Stefan_Moonen</h2>
+		<h3>Quiz koning 1'</h3>
+
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Violette_Muller.jpg')" alt='sietse'></div>
+		<h2>Violette_Muller</h2>
+		<h3>Quiz koning 2'</h3>
+	</div>
+	<div class='headrank'>
+		<div class='head' style="background-image:url('img/Sjoerd_Eikenaar.jpg')" alt='sietse'></div>
+		<h2>Sjoerd_Eikenaar</h2>
+		<h3>Quiz koning 3'</h3>
+	</div>
+</section>
